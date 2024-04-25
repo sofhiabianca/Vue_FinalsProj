1e2fc6dc9839f0c4914d08ea47dbfa555a5ac293
chaclaire <118405013+chaclaire@users.noreply.github.com>
2024-04-25 08:44:42 +0800

    edit charm bg

--- src/views/Charm.vue
+++ src/views/Charm.vue
@@ -6,5 +6,23 @@ import { RouterLink } from 'vue-router'
 <template>
     <Nav/>
 <RouterView/>
+<div class="container">
+      <div class="image"> <img src="../images/bg1.jpg" /> </div> 
+      <div class="image"> <img src="../images/bg2.jpg" /> </div> 
+      <div class="image"> <img src="../images/bg3.jpg" /> </div> 
 
+    </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center; 
+  justify-content: center;
+}
+img{
+  height:515px;
+  margin-left: 15px ;
+  margin-right: 15px;
+}
+</style>
